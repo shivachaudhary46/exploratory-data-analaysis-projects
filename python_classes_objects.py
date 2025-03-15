9ba6cb45f7ef702b78b87c6b220db8d0236667ae
shivachaudhary46 <shivachaudhary4116@gmail.com>
2025-03-15 20:07:52 +0545

    python class and objects

--- python_classes_objects.py
+++ python_classes_objects.py
@@ -1,12 +1,31 @@
 
+# class snake:
+#     name = "anaconda"
+
+#     #method to change name 
+#     def modifyName(self, new_name):
+#         self.name = new_name
+
+# snake1 = snake()
+# print(snake1.name)
+# snake1.modifyName('cobra')
+# print(snake1.name)
+
 class snake:
-    name = "anaconda"
+    
+    def __init__(self, name, id):
+        self.name = name
+        self.id = id
 
-    #method to change name 
-    def modifyName(self, new_name):
-        self.name = new_name
+    def modifyName(self, newName):
+        self.name = newName
 
-snake1 = snake()
+snake1 = snake('anaconda', 56)
+snake2 = snake('python', 89)
+
 print(snake1.name)
-snake1.modifyName('cobra')
-print(snake1.name)
+print(snake1.id)
+
+print(snake2.name)
+print(snake2.id)
+
