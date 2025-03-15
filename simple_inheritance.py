3896f5e81bb5533a9877affb3418515e4b07e17d
shivachaudhary46 <shivachaudhary4116@gmail.com>
2025-03-15 22:06:53 +0545

    inheritance example

---
+++ simple_inheritance.py
@@ -0,0 +1,26 @@
+#Person parent class 
+class Person:
+    def person_data(self, name, age):
+       
+        print("hello from the person class")
+        print("the name is: ", name, "and the age is ", age)
+
+#company parent class 
+class Company:
+    def company_data(self, comp_name, location):
+        
+        print("welcom to the company !")
+        print("The company name is ", comp_name, "located in ", location)
+
+#Employee child class 
+class Employee(Person, Company):
+    def employee_details(self, salary, skill):
+        print("welcome to the employee class ")
+        print("the employee has skill ", skill, "salary of ", salary)
+
+# objects for employee
+Employee1 = Employee()
+
+Employee1.person_data('shiva', 19)
+Employee1.company_data('fusemachines', 'jawalakhel')
+Employee1.employee_details(500000, 'machine learning engineer')
