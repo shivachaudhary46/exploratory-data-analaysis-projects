1f64391635f6a43aa9143e7f7aaea391c6cb710f
shivachaudhary46 <shivachaudhary4116@gmail.com>
2025-03-18 23:47:06 +0545

    stack implementation using List

---
+++ stack/Stack_implementation_using_list.py
@@ -0,0 +1,15 @@
+#python program to implement 
+#stack using List 
+
+stack = []
+
+# append() function to push
+# element in the stack 
+stack.append('a')
+stack.append('b')
+stack.append('c')
+stack.append('d')
+
+#taking out the pushed element 
+#Filo order 
+print(stack.pop())
