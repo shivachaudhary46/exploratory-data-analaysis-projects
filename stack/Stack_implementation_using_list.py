80eb94957682a556de348c4609b7f001c9e5702d
shivachaudhary46 <shivachaudhary4116@gmail.com>
2025-03-19 17:16:28 +0545

    stack implmentation using list

--- stack/Stack_implementation_using_list.py
+++ stack/Stack_implementation_using_list.py
@@ -10,6 +10,14 @@ stack.append('b')
 stack.append('c')
 stack.append('d')
 
+print('initial stack : ', stack)
+
 #taking out the pushed element 
 #Filo order 
 print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+
+print('after poping out all element : ')
+print(stack)
